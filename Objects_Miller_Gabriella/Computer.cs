4be8718bb940d12f9cdb34ba228cf65301da09c6
Gabby <gabbie10112005@gmail.com>
2025-01-30 10:32:34 -0600

    a

--- Objects_Miller_Gabriella/Computer.cs
+++ Objects_Miller_Gabriella/Computer.cs
@@ -12,14 +12,35 @@ namespace Objects_Miller_Gabriella
 
         public string Brand { get; set; }
 
+        public string Type { get; set; }
 
+        public Computer(string brand, string type, bool isOn)
+        {
+            Brand = brand;
+            Type = type;
+            IsOn = isOn;
+        }
 
 
         public void PowerOnOff()
         {
-            
 
+            IsOn = !IsOn; 
 
+            if (IsOn == true)
+            {
+                Console.WriteLine("The computer is booting up!");
+            }
+            else
+            {
+                Console.WriteLine("The computer is shutting down!");
+            }
         }
+
+        public override string ToString()
+        {
+            return $"The computer is made by {Brand} and is a(n) {Type} computer, it is on: {IsOn}!";
+        }
+
     }
 }
--- Objects_Miller_Gabriella/Program.cs
+++ Objects_Miller_Gabriella/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Computer myComputer = new Computer("Apple", "All-in-One", true);
+            IBootUp myBootUp = myComputer;
+            Console.WriteLine(myComputer.Equals(myBootUp));
+
+            Computer myOtherComputer = new Computer("Apple", "All-in-One", true);
+            Console.WriteLine(myComputer.Equals(myOtherComputer));
+
+            string myString = myComputer.ToString();
+            string myOtherString = myString;
+
+            myString = "Hi!";
+
+            Console.WriteLine(myString);
+            Console.WriteLine(myOtherString);
+
+
+            Console.WriteLine(myString == myComputer.ToString());
+            Console.WriteLine(myString == myOtherComputer.ToString());
+
+            Console.WriteLine(myComputer.GetType());
+            Console.WriteLine(myBootUp.GetType());
+
+            myOtherComputer.PowerOnOff();
+
+            Console.WriteLine(myString == myOtherComputer.ToString());
+            Console.WriteLine(myString);
+            Console.WriteLine(myOtherComputer.ToString());
+
+            Console.WriteLine(myString.Contains('i'));
+
+            Console.WriteLine(myString.ToUpper());
         }
     }
 }
